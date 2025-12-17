b10bf831ca335763eb038b7025d330eed88d5bd6
taji <taji@sonicgarden.jp>
2025-12-17 13:22:42 +0900

    fetchを利用する

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-import { setupWeatherForm } from './api.js';
 
-setupWeatherForm();
+document.getElementById('weather-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const cityCode = document.getElementById('city-code').value;
+  const apiUrl = `https://weather.tsukumijima.net/api/forecast/city/${cityCode}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => { console.log(data.publicTime);}
+  );
+});
