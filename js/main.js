308270ea946f4bd246be09a1a2144470a962bbec
taji <taji@sonicgarden.jp>
2025-12-17 23:03:44 +0900

    try, catch

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ import { fetchWeatherDate } from './api.js';
 document.getElementById('weather-form').addEventListener('submit', async(e) => {
   e.preventDefault();
   const cityCode = document.getElementById('city-code').value;
-  const weatherData = await fetchWeatherDate(cityCode)
-  console.log(weatherData);
+
+// fetchはネットワークエラーの時だけcatchで捕まえられる。
+// 400, 500系のエラーはcatchで捕まえられないので注意。
+// とりあえず、catchで捕まえる方法考える
+
+  try {
+    const weatherData = await fetchWeatherDate(cityCode)
+    console.log(weatherData);
+  } catch (error) {
+    console.log('失敗したぜおら');
+    console.log(error);
+  }
 })
