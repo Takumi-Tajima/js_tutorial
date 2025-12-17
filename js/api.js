c64561ce1a55d628c77cf00ebbe16660c5d2dc1c
taji <taji@sonicgarden.jp>
2025-12-17 21:40:56 +0900

    別ファイルに

--- js/api.js
+++ js/api.js
@@ -1,7 +1,12 @@
 export function setupWeatherForm() {
   document.getElementById('weather-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const cityCode = document.getElementById('city-code').value;
-    window.location.href = `https://weather.tsukumijima.net/api/forecast/city/${cityCode}`;
+  e.preventDefault();
+  const cityCode = document.getElementById('city-code').value;
+  const apiUrl = `https://weather.tsukumijima.net/api/forecast/city/${cityCode}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => { console.log(data);}
+  );
   });
 }
--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
+import { setupWeatherForm } from './api.js';
 
-document.getElementById('weather-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const cityCode = document.getElementById('city-code').value;
-  const apiUrl = `https://weather.tsukumijima.net/api/forecast/city/${cityCode}`;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => { console.log(data.publicTime);}
-  );
-});
+setupWeatherForm();
